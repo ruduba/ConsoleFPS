a8231e95215727f62cea320c7f9c92807a5f9afc
ruduba <vatbudur@gmail.com>
2025-12-08 15:07:29 +0530

    Added shading wrt to distance to wall

--- source.cpp
+++ source.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include<chrono>
 #include <iostream>
 using namespace std;
 #define _UNICODE
@@ -23,7 +24,7 @@ int main()
 wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
 HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 SetConsoleActiveScreenBuffer(hConsole);
-DWORD dwBytesWritten =0;
+DWORD dwBytesWritten = 0;
 
 wstring map;
 map += L"################"; // top wall
@@ -43,16 +44,25 @@ map += L"#..............#";
 map += L"#..............#";
 map += L"################"; // bottom wall
 
+auto tp1 = chrono::system_clock::now();
+auto tp2 = chrono::system_clock::now();
+
+
 //game loop
 while(1){
+
+    tp2 = chrono::system_clock::now();
+    chrono::duration<float> elapsedTime = tp2-tp1;
+    tp1 = tp2;
+    float fElapsedTime = elapsedTime.count();
 
     //controls
     //handle CCW Rotation
     if(GetAsyncKeyState((unsigned short)'A')& 0x8000)
-        fPlayerA -= (0.1f);
+        fPlayerA -= (0.1f) * fElapsedTime;
 
     if(GetAsyncKeyState((unsigned short)'D')& 0x8000)
-        fPlayerA += (0.1f);
+        fPlayerA += (0.1f)* fElapsedTime;
 
 
 
@@ -92,11 +102,23 @@ while(1){
             if(nCeiling < 0) nCeiling = 0;
             if(nFloor >= nScreenHeight) nFloor = nScreenHeight - 1;
 
+        ///*
+        short nShade = ' ';
+
+        if(fDistanceToWall <= fDepth/4.0f) nShade = 0x2588;
+        else if(fDistanceToWall < fDepth / 3.0f) nShade = 0x2593;
+        else if(fDistanceToWall < fDepth / 2.0f) nShade = 0x2592;
+        else if(fDistanceToWall < fDepth) nShade = 0x2591;
+        else nShade = ' ';
+        //*/
+
+
+
         for(int y = 0; y< nScreenHeight; y++)
             if(y <= nCeiling)
                 screen[y*nScreenWidth + x] = ' ';
             else if(y > nCeiling && y <= nFloor)
-                screen[y*nScreenWidth + x] = '#';
+                screen[y*nScreenWidth + x] = nShade;
             else
                 screen[y*nScreenWidth + x] = ' ';
     }
